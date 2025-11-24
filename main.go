a11981d87b5e1d5b6cca94315d145a56923b9617
Peter Aronoff <peter@aronoff.org>
2025-11-24 07:50:46 -0500

    Add a brief blurb to main.go.

--- main.go
+++ main.go
@@ -1,4 +1,12 @@
-// gitmirror uses git commands to backup git repositories.
+// gitmirror uses git itself to back up git repositories.
+//
+// Briefly stated, gitmirror uses two commands.
+//
+//	git clone --mirror <gitURL>
+//	git remote update <gitURL>
+//
+// The first command creates an initial archive of the repository; the second
+// brings the archive up to date.
 package main
 
 import (
