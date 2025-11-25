55bc43fbc5335617df21a5058176d789f0cce6cd
Peter Aronoff <peter@aronoff.org>
2025-11-25 11:07:44 -0500

    Improve documentation
    1. Clean up the style of the CLI usage.
    2. Hide README.md from godoc.

--- internal/cli/usage.go
+++ internal/cli/usage.go
@@ -1,14 +1,16 @@
 package cli
 
-var cmdUsage = `usage: gitmirror [options]
+var cmdUsage = `Use git commands to clone and update backups of git repositories.
 
-Use git commands to clone and update backups of git repositories.
+Usage:
 
-Options
+        gitmirror [--config FILE] [--quiet] [-h|--help] [--version]
+
+Options:
         --config=FILE   Use FILE as config file (default ~/.gitmirror.json)
         --quiet         Print only error messages
 
     -h, --help          Print this help and exit
         --version       Print version and exit
 
-For more information or to file an issue, visit https://github.com/telemachus/gitmirror`
+Source code: https://github.com/telemachus/gitmirror`
